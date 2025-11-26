b505d51883862b340985678379dccafda72b96d8
SNatty98 <mrnatt98@gmail.com>
2025-11-26 20:51:36 +0000

    Serializer created

---
+++ backend/scheduler/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from .models import User, Substitute, Assignment, Application
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'role']
+        read_only_fields = ['id']
+
+
+class SubstituteSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    full_name = serializers.CharField(
+        source='user.get_full_name', readonly=True)
+
+    class Meta:
+        model = Substitute
+        fields = '__all__'
+
+
+class AssignmentListSerializer(serializers.ModelSerializer):
+    application_count = serializers.IntegerField(
+        source='applications.count', read_only=True)
+    selected_substitute_name = serializers.CharField(
+        source='selected_substitute.user.get_full_name',
+        read_only=True,
+        allow_null=True
+    )
+
+    class Meta:
+        model = Assignment
+        fields = '__all__'
+
+
+class AssignmentDetailSerializer(serializers.ModelSerializer):
+    applications = serializers.SerializerMethodField()
+    selected_substitute = SubstituteSerializer(read_only=True)
+
+    class Meta:
+        model = Assignment
+        fields = '__all__'
+
+    def get_applications(self, obj):
+        applications = obj.applications.select_related('substitute__user')
+        return ApplicationSerializer(applications, many=True).data
+
+
+class ApplicationSerializer(serializers.ModelSerializer):
+    substitute = SubstituteSerializer(read_only=True)
+    assignment_details = AssignmentListSerializer(
+        source='assignment', read_only=True)
+
+    class Meta:
+        model = Application
+        fields = '__all__'
+        read_only_fields = ['substitute', 'applied_at', 'updated_at']
